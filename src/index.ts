705d741bdded835dce9e38e46e402ff43df72fdc
Sergio Cinos <sergio.cinos@gmail.com>
2021-01-16 18:01:22 +0100

    Add pm2 config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { app } from './app';
 import http from 'http';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 http.createServer(app.callback()).listen(PORT, () => {
     console.log(`Server is running!`);
