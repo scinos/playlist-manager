abe194ed82a2ec4255204ba022a25e24bfea193e
Sergio Cinos <sergio.cinos@gmail.com>
2021-01-16 07:22:48 +0100

    Isolate eslint config for TS

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,17 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        project: './tsconfig.eslint.json',
-    },
     env: {
         node: true,
     },
+    overrides: [
+        {
+            files: ['*.ts'],
+            parser: '@typescript-eslint/parser',
+            parserOptions: {
+                ecmaVersion: 2020,
+                sourceType: 'module',
+                project: './tsconfig.json',
+            },
+        },
+    ],
     extends: ['eslint:recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
 };
